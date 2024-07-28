526de408402b9c213f3bf1432cce2f142af7bf4f
Vinicius <viniciustfaraujo@gmail.com>
2024-07-28 20:27:12 -0300

    feat: add sign in and sign up screens

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+Color backgroundColor1 = const Color(0xffE9EAF7);
+Color backgroundColor2 = const Color(0xffF4EEF2);
+Color backgroundColor3 = const Color(0xffEBEBF2);
+Color backgroundColor4 = const Color(0xffE3EDF5);
+Color backgroundColor5 = Color.fromARGB(246,246,246,255);
+Color textColor1 = const Color(0xff353047);
+Color textColor2 = const Color(0xff6F6B7A);
+Color buttonColor = Color.fromARGB(255, 45, 197, 91);
