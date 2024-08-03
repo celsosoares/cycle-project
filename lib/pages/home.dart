4366461ee0532c97b69b6a69e0d5f25a52d212f0
vinicius ferreira <viniciusferreira@cers.com.br>
2024-08-03 02:05:04 -0300

    Mais icones e home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,24 +4,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue, // Cor de fundo da tela de splash
+      appBar: AppBar(
+        title: Text('Olá, Vinícius'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              Navigator.of(context).pushNamed('/sign-in');
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text('Meus pontos'), // Substitua pelo nome do usuário
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-                'images/luffy.jpg'), // Substitua pelo caminho da sua imagem
-            SizedBox(height: 16),
-            Text(
-              'HOMEEEEEEEEEE',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            Card(
+              elevation: 4,
+              child: Column(
+                children: [
+                  Image.asset('images/luffy.jpg',
+                      fit: BoxFit.contain), // Substitua pelo caminho da imagem
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Conteúdo do card',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
               ),
             ),
+            // Outros widgets aqui
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[850],
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('images/crafts.png', fit: BoxFit.contain),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Buscar',
+          ),
+          // Adicione mais itens conforme necessário
+        ],
       ),
     );
   }
