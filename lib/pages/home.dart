03c9e03781912f4a1d616bb4d065c99c1be175bd
vinicius ferreira <viniciusferreira@cers.com.br>
2024-08-03 02:53:17 -0300

    Home com lista e scroll de cards

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final List<Product> products = [
+    Product('Camisa preta', 'R\$29,90', 'Lorena Silva', 5, 'images/clothes.png'),
+    Product('Acessorios', 'R\$45,00', 'Camila Souza', 5, 'images/accessories.png'),
+    Product('Sapatos', 'R\$35,00', 'Camila Souza', 5, 'images/shoes.png'),
+    Product('Guarda-chuva', 'R\$40,00', 'Camila Souza', 5, 'images/bags.png'),
+    Product('Pneu', 'R\$40,00', 'Camila Souza', 5, 'images/google.png'),
+    Product('Decoração', 'R\$40,00', 'Camila Souza', 5, 'images/furniture.png'),
+    Product('Prato', 'R\$40,00', 'Camila Souza', 5, 'images/crafts.png'),
+    Product('Maça', 'R\$40,00', 'Camila Souza', 5, 'images/apple.png'),
+    Product('Porta', 'R\$40,00', 'Camila Souza', 5, 'images/utensils.png'),
+    Product('Televisão', 'R\$40,00', 'Camila Souza', 5, 'images/facebook.png'),
+    // Adicione outros produtos seguindo a mesma estrutura
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,27 +34,32 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Card(
-              elevation: 4,
-              child: Column(
-                children: [
-                  Image.asset('images/luffy.jpg',
-                      fit: BoxFit.contain), // Substitua pelo caminho da imagem
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Conteúdo do card',
-                      style: TextStyle(fontSize: 18),
-                    ),
+        child: Container(
+          height: 400, // Defina a altura desejada para o card
+          child: ListView.builder(
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              return Card(
+                shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black), // Defina a cor da borda
+                borderRadius: BorderRadius.circular(10),),
+                child: ListTile(
+                  leading: Image.network(products[index].image), // Substitua pela URL real da imagem
+                  title: Text(products[index].name),
+                  subtitle: Text(products[index].seller),
+                  trailing: Column(
+                    children: <Widget>[
+                      Text(products[index].price),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(5, (i) => Icon(Icons.star_border)),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            // Outros widgets aqui
-          ],
+                ),
+              );
+            },
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -63,4 +82,17 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+class Product {
+  String name;
+  String price;
+  String seller;
+  int rating;
+  String image;
+
+  // Adicione imageUrl se você tiver imagens hospedadas online.
+  // String imageUrl;
+
+  Product(this.name, this.price, this.seller, this.rating, this.image);
 }
