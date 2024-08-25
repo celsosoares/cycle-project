ba06017191ef0703f425e2e52974e2eba30404de
Vinicius <viniciustfaraujo@gmail.com>
2024-08-24 23:36:47 -0300

    add initial text to donation screen

--- lib/pages/donation.dart
+++ lib/pages/donation.dart
@@ -7,24 +7,37 @@ class DonationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 	return Scaffold(
 	  appBar: AppBar(
-		title: Text('Donation Screen'),
+		title: Text('Como você gostaria de doar?'),
 	  ),
-	  body: Center(
-		child: Container(
-		  width: 100,
-		  height: 100,
-		  color: Colors.green,
-		  child: Center(
-			child: Text(
-			  'Sample Text',
-			  style: TextStyle(
-				color: Colors.white,
-				fontSize: 16,
+	  body: Column(
+		mainAxisAlignment: MainAxisAlignment.start,
+		children: [
+		  Center(
+			child: Padding(
+			  padding: EdgeInsets.symmetric(horizontal: 16.0), // Add horizontal padding
+			  child: Container(
+				decoration: BoxDecoration(
+				  color: Colors.green,
+				  borderRadius: BorderRadius.circular(20),
+				),
+				padding: EdgeInsets.all(16.0),
+				child: Text(
+				  'Como funciona?\n'
+				  'Caso você tenha algum material que queira doar, selecione Quero doar um material específico e veja quais designers estão precisando desse material.\n\n'
+				  'Caso você conheça ou se interessou por algum designer da plataforma, selecione Quero doar para um designer específico e veja quais materiais esse designer precisa.\n\n'
+				  'Após confirmar o pedido de doação, entre em contato com o designer para a entrega dos materiais, você receberá os pontos quando o designer receber e validar a doação.',
+				  style: TextStyle(
+					color: Colors.white,
+					fontSize: 16,
+				  ),
+				  textAlign: TextAlign.center,
+				  maxLines: null,
+				  overflow: TextOverflow.visible,
+				),
 			  ),
-			  textAlign: TextAlign.center,
 			),
 		  ),
-		),
+		],
 	  ),
 	);
   }
