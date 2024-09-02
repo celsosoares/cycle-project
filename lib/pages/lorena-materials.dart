13adbbfe0636e8b3e685198c205ce891a4c8c609
Vinicius <viniciustfaraujo@gmail.com>
2024-09-01 22:05:55 -0300

    add images and replace placeholder text

--- lib/pages/lorena-materials.dart
+++ lib/pages/lorena-materials.dart
@@ -1,40 +1,50 @@
 import 'package:flutter/material.dart';
 
-
 class LorenaMaterials extends StatelessWidget {
   static const routeName = '/LorenaMaterialsScreen';
 
-
-  @override 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lorena Materials'),
+        title: Text('Doação'),
       ),
       body: ListView(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                'Materiais desejados por Lorena',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Card(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset('images/lorena.png'),
+                  Image.asset('images/sheets.png'),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      'Bold Title',
+                      'Lençol',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text('Subtitle'),
+                    child: Text('Tecidos'),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      '40 points',
+                      '40 pontos',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
@@ -49,22 +59,52 @@ class LorenaMaterials extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset('images/lorena.png'),
+                  Image.asset('images/newspaper.png'),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      'Another Bold Title',
+                      'Jornal',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text('Another Subtitle'),
+                    child: Text('Papel'),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      '50 points',
+                      '50 pontos',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 16.0), // Add space between cards
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('images/jeans.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Jeans',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Tecido'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '20 pontos',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
